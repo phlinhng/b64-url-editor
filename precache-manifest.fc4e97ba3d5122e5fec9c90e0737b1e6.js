db398df8c945d08673ea68be8870573f2618da61
phlinhng <phlinhng@outlook.com>
2020-07-11 02:32:37 +0800

    Updates

--- precache-manifest.fc4e97ba3d5122e5fec9c90e0737b1e6.js
+++ precache-manifest.fc4e97ba3d5122e5fec9c90e0737b1e6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7dd604ef39cba8221a4a7989c23deeb4",
+    "revision": "5ce726610ddb4c3717a6921277877f4f",
     "url": "/b64-url-editor/index.html"
   },
   {
-    "revision": "57bad3219cd8ba401ff6",
+    "revision": "5e3e292c65ef61590089",
     "url": "/b64-url-editor/static/css/2.8f00e7e6.chunk.css"
   },
   {
-    "revision": "4acd8937061d45682b84",
+    "revision": "9435716489a112e4efc7",
     "url": "/b64-url-editor/static/css/main.aa0e9227.chunk.css"
   },
   {
-    "revision": "57bad3219cd8ba401ff6",
-    "url": "/b64-url-editor/static/js/2.1f18dc41.chunk.js"
+    "revision": "5e3e292c65ef61590089",
+    "url": "/b64-url-editor/static/js/2.e92f8e8b.chunk.js"
   },
   {
     "revision": "b9f28c176efdd716457c02939f4eac67",
-    "url": "/b64-url-editor/static/js/2.1f18dc41.chunk.js.LICENSE.txt"
+    "url": "/b64-url-editor/static/js/2.e92f8e8b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4acd8937061d45682b84",
-    "url": "/b64-url-editor/static/js/main.5d88b069.chunk.js"
+    "revision": "9435716489a112e4efc7",
+    "url": "/b64-url-editor/static/js/main.a95e7f3e.chunk.js"
   },
   {
     "revision": "1f69d919defd4941487d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/b64-url-editor/precache-manifest.03af68da90528744399aba12b9f10a66.js"
+  "/b64-url-editor/precache-manifest.fc4e97ba3d5122e5fec9c90e0737b1e6.js"
 );
 
 self.addEventListener('message', (event) => {
